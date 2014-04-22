47b8551c5ab79a167ac697150748cb219ff9751d
Raphael Pierzina <raphael@hackebrot.de>
2014-04-22 22:44:18 +0200

    Remove deprecated name testing

--- {{cookiecutter.repo_name}}/main.py
+++ {{cookiecutter.repo_name}}/main.py
@@ -6,7 +6,6 @@ kivy.require('{{ cookiecutter.minimum_kivy_version }}')
 
 from {{ cookiecutter.repo_name }}.{{ cookiecutter.repo_name }}app import {{ cookiecutter.repo_name|capitalize }}App
 
-
-if __name__ in ('__main__', '__android__'):
+if __name__ == "__main__":
     {{ cookiecutter.repo_name|capitalize }}App().run()
 

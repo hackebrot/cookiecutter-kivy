d1ed40a7aea38dd19a144ad3f4a6f40e41ce0f95
Raphael Pierzina <raphael@hackebrot.de>
2015-03-03 21:22:12 +0100

    Clean up source code of the remaining files

--- {{cookiecutter.repo_name}}/main.py
+++ {{cookiecutter.repo_name}}/main.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import kivy
-kivy.require('{{ cookiecutter.minimum_kivy_version }}')
+kivy.require('{{cookiecutter.minimum_kivy_version}}')
 
-from {{ cookiecutter.repo_name }}.{{ cookiecutter.repo_name }}app import {{ cookiecutter.repo_name|capitalize }}App
+from {{cookiecutter.repo_name}}.{{cookiecutter.repo_name}}app import {{cookiecutter.repo_name|capitalize}}App
 
 if __name__ == "__main__":
-    {{ cookiecutter.repo_name|capitalize }}App().run()
-
+    {{cookiecutter.repo_name|capitalize}}App().run()
